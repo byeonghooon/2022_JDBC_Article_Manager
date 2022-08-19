da4eaa547c3d3a68719c3753bd2461394aa4b70a
byeonghunki <orangeys1000@gmail.com>
2022-08-19 14:58:32 +0900

    게시물 수정,삭제시 권한 체크

--- src/com/KoreaIT/example/JAM/controller/ArticleController.java
+++ src/com/KoreaIT/example/JAM/controller/ArticleController.java
@@ -44,10 +44,14 @@ public class ArticleController extends Controller {
 
 		int id = Integer.parseInt(cmd.split(" ")[2]);
 
-		boolean isArticleExists = articleService.isArticleExists(id);
+		Article article = articleService.getArticleById(id);
 
-		if (isArticleExists == false) {
+		if (article == null) {
 			System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+			return;
+		}
+		if (article.memberId != Container.session.loginedMemberId) {
+			System.out.println("해당 게시글에 대한 권한이 없습니다.");
 			return;
 		}
 
@@ -96,6 +100,10 @@ public class ArticleController extends Controller {
 
 		if (article == null) {
 			System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+			return;
+		}
+		if (article.memberId != Container.session.loginedMemberId) {
+			System.out.println("해당 게시글에 대한 권한이 없습니다.");
 			return;
 		}
 
