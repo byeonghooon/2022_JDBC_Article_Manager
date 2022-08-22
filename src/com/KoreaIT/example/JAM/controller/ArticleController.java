b98f8826421571fdb68d79c62641021754851f56
byeonghunki <orangeys1000@gmail.com>
2022-08-22 14:37:10 +0900

    게시물 리스트 페이징, 검색

--- src/com/KoreaIT/example/JAM/controller/ArticleController.java
+++ src/com/KoreaIT/example/JAM/controller/ArticleController.java
@@ -121,7 +121,24 @@ public class ArticleController extends Controller {
 	public void showList(String cmd) {
 		System.out.println("== 게시물 리스트 ==");
 
-		List<Article> articles = articleService.getArticles();
+		String[] cmdBits = cmd.split(" ");
+
+		int page = 1;
+		String searchKeyword = "";
+
+		if (cmdBits.length >= 3) {
+			page = Integer.parseInt(cmdBits[2]);
+		}
+
+		if (cmdBits.length >= 4) {
+			searchKeyword = cmdBits[3];
+		}
+
+		int itemsInAPage = 10;
+
+
+		List<Article> articles = articleService.getForPrintArticles(page, itemsInAPage, searchKeyword);
+		
 
 		if (articles.size() == 0) {
 			System.out.println("게시물이 없습니다");
